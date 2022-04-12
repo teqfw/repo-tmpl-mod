53739d8293d667534a12936b9296c4f91be019f7
Alex Gusev <alex@flancer64.com>
2022-04-12 06:55:04 +0300

    WF-581 Split web plugin to parts

--- src/Back/Plugin/Stop.mjs
+++ src/Back/Plugin/Stop.mjs
@@ -1,18 +1,24 @@
 /**
  * Plugin finalization function.
+ * @namespace TeqFw_Plugin_Back_Plugin_Stop
  */
 // MODULE'S VARS
 const NS = 'TeqFw_Plugin_Back_Plugin_Stop';
 
-export default function Factory(spec) {
+export default function (spec) {
     // EXTRACT DEPS
+    /** @type {TeqFw_Core_Shared_Api_ILogger} */
+    const logger = spec['TeqFw_Core_Shared_Api_ILogger$$']; // instance
 
-    // COMPOSE RESULT
+    // FUNCS
+    /**
+     * @return {Promise<void>}
+     * @namespace TeqFw_Plugin_Back_Plugin_Stop
+     */
     async function action() { }
 
-    Object.defineProperty(action, 'name', {value: `${NS}.${action.name}`});
+    // MAIN
+    logger.setNamespace(NS);
+    Object.defineProperty(action, 'namespace', {value: NS});
     return action;
 }
-
-// finalize code components for this es6-module
-Object.defineProperty(Factory, 'name', {value: `${NS}.${Factory.name}`});
